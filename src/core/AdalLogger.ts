7237e154173b36e503239e1c985ddbac9c36c0ad
Brad Hughes <bradhugh@microsoft.com>
2019-04-04 13:33:18 -0400

    Add additional intelligence to logger

--- src/core/AdalLogger.ts
+++ src/core/AdalLogger.ts
@@ -3,63 +3,89 @@ import { ICoreLogger } from "./CoreLoggerBase";
 // tslint:disable: no-console
 
 export class AdalLogger implements ICoreLogger {
-    public piiLoggingEnabled: boolean;
+    public piiLoggingEnabled: boolean = false;
 
     constructor(public correlationId: string) {}
 
     public error(messageScrubbed: string): void {
-        console.log(messageScrubbed);
+        console.log(`[ELOAS] [ERROR] ${messageScrubbed}`);
     }
 
     public errorPii(messageWithPii: string): void {
-        console.log(messageWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log(`[ELOAS] [ERROR] ${messageWithPii}`);
+        }
     }
 
     public errorExPii(exWithPii: Error): void {
-        console.log(exWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log("[ELOAS] [ERROR]");
+            console.log(exWithPii);
+        }
     }
 
     public errorExPiiWithPrefix(exWithPii: Error, prefix: string): void {
-        console.log(exWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log(`[ELOAS] [ERROR] ${prefix}`);
+            console.log(exWithPii);
+        }
     }
 
     public warning(messageScrubbed: string): void {
-        console.log(messageScrubbed);
+        console.log(`[ELOAS] [WARN] ${messageScrubbed}`);
     }
 
     public warningPii(messageWithPii: string): void {
-        console.log(messageWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log(`[ELOAS] [WARN] ${messageWithPii}`);
+        }
     }
 
     public warningExPii(exWithPii: Error): void {
-        console.log(exWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log("[ELOAS] [WARN]");
+            console.log(exWithPii);
+        }
     }
 
     public warningExPiiWithPrefix(exWithPii: Error, prefix: string): void {
-        console.log(exWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log(`[ELOAS] [WARN] ${prefix}`);
+            console.log(exWithPii);
+        }
     }
 
     public info(messageScrubbed: string): void {
-        console.log(messageScrubbed);
+        console.log(`[ELOAS] [INFO] ${messageScrubbed}`);
     }
 
     public infoPii(messageWithPii: string): void {
-        console.log(messageWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log(`[ELOAS] [INFO] ${messageWithPii}`);
+        }
     }
 
     public infoExPii(exWithPii: Error): void {
-        console.log(exWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log("[ELOAS] [INFO]");
+            console.log(exWithPii);
+        }
     }
 
     public infoExPiiWithPrefix(exWithPii: Error, prefix: string): void {
-        console.log(exWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log(`[ELOAS] [INFO] ${prefix}`);
+            console.log(exWithPii);
+        }
     }
 
     public verbose(messageScrubbed: string): void {
-        console.log(messageScrubbed);
+        console.log(`[ELOAS] [VERBOSE] ${messageScrubbed}`);
     }
 
     public verbosePii(messageWithPii: string): void {
-        console.log(messageWithPii);
+        if (this.piiLoggingEnabled) {
+            console.log(`[ELOAS] [VERBOSE] ${messageWithPii}`);
+        }
     }
 }
